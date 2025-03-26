1f005453275429c2f9258fb2f19e41250fc718bb
NidhieKapletiya <nidhie09@gmail.com>
2025-03-26 17:33:08 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,6 +29,10 @@ st.dataframe(sales_by_month)
 # Here the grouped months are the index and automatically used for the x axis
 st.line_chart(sales_by_month, y="Sales")
 
+# Convert 'Order_Date' column to datetime format and extract the month
+
+df['Month'] = df['Order_Date'].dt.strftime('%B')  # Extract month
+
 #st.write("## Your additions")
 #st.write("### (1) add a drop down for Category (https://docs.streamlit.io/library/api-reference/widgets/st.selectbox)")
 
@@ -58,9 +62,6 @@ filtered_data = filtered_by_category[filtered_by_category['Sub_Category'].isin(s
 
 st.write("### (3) show a line chart of sales for the selected items in (2)")
 
-# Convert 'Order_Date' column to datetime format and extract the month
-df['Order_Date'] = pd.to_datetime(df['Order_Date'])
-df['Month'] = df['Order_Date'].dt.strftime('%B')  # Extract month
 
 # Line chart for selected subcategory
 if not filtered_data.empty:
